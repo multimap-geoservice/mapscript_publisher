070669a79ab62d53bf533c54eda816b5139f1ef2
oldbay <old_bay@mail.ru>
2019-03-25 23:40:09 +0300

    new query for landuse

---
+++ _prototype/sql/landuse_select.sql
@@ -0,0 +1,38 @@
+﻿select way as geometry,
+osm_id, 
+concat(
+    landuse, 
+    "natural", 
+    leisure
+) as type, 
+name as name 
+from planet_osm_polygon 
+where landuse in 
+(
+    'forest',
+     'wood',
+     'pedestrian',
+     'cemetery',
+     'industrial',
+     'commercial',
+     'brownfield',
+     'residential',
+     'school',
+     'college',
+     'university',
+     'military',
+     'park',
+     'golf_course',
+     'hospital',
+     'parking',
+     'stadium',
+     'sports_center',
+     'pitch'
+) 
+or "natural" in ('wood') 
+or leisure in 
+(
+     'park',
+     'golf_course' 
+) 
+order by area desc
