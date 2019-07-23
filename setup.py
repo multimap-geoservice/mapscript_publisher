f0822516afb3298a8cebd09f9e96310304d72358
oldbay <old_bay@mail.ru>
2019-07-23 14:42:24 +0300

    v0.2

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='mapscript_publisher', 
-    version='0.1', 
+    version='0.2', 
     packages=['map_pub'], 
     install_requires=[
         'mapscript', 
