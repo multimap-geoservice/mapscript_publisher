da6566f86c7d79b6fe2ed37b781efb5874e6c151
oldbay <old_bay@mail.ru>
2019-09-30 19:47:36 +0300

    next next sxf map, to do

---
+++ maps/sql/sxf_union_function.sql
@@ -0,0 +1,58 @@
+﻿
+DROP FUNCTION sxf_union(text,text[],integer[]);
+create or replace function sxf_union(
+    geom_type text,
+    layer_name text[],
+    map_ids int[]
+    )
+returns table (
+    gid integer,
+    code integer,
+    rsc_code integer,
+    key character varying,
+    name character varying,
+    layername character varying,
+    botscale integer,
+    topscale integer,
+    attr1 character varying,
+    attr2 character varying,
+    attr3 character varying,
+    attr4 character varying,
+    attr5 character varying,
+    map_id integer,
+    wkb_geometry geometry
+) as $$
+    declare
+        all_tables text[];
+        table_name text;
+        query_array text[];
+        query_str text;
+    begin
+        execute'
+            select array_agg(table_name::text)
+            from information_schema.tables
+            where table_type=''BASE TABLE''
+            and table_schema=''public''
+            and (regexp_split_to_array(table_name, ''_''))[4] = '''||geom_type||''';'
+        into all_tables;
+
+        foreach table_name in array all_tables
+        loop
+            query_array := array_append(
+                query_array,'
+                select *
+                from '||table_name||' as geodata
+                where layername = any(array['''||array_to_string(layer_name,''',''')||'''])
+                and map_id in (
+                    select map_id 
+                    from sxf_common_loaded_maps 
+                    where external_id = any(array['||array_to_string(map_ids,',')||'])
+                )
+                '
+            );
+        end loop;
+        query_str := array_to_string(query_array, 'union');
+        
+        return query execute query_str;
+    end;
+$$ language plpgsql
--- run_sxf.py
+++ run_sxf.py
@@ -11,7 +11,7 @@ if __name__ == "__main__":
     mapjsonfile = "./maps/geodb_sxf.json"
     db_host = sys.argv[1]
     #db_host = "localhost"
-    db_data = '(9511,9512,9513,9514,9515,9516,9517,9518,9519,9520,9521,9522,9523,9524,9525,9526,9527,9528,9529,9530)'
+    db_data = '9511,9512,9513,9514,9515,9516,9517,9518,9519,9520,9521,9522,9523,9524,9525,9526,9527,9528,9529,9530'
     debug_path = "{}/GIS/mapserver/debug".format(os.environ["HOME"])
     db_connection = "host={0} dbname=sxf user=innouser password=innopass port=5432".format(
         db_host 
